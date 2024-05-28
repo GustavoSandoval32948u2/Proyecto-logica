a8c4f9a17304bc8c4af9dcf5cc83e9564c14f2e8
Gustavo <gsandovals@miumg.edu.gt>
2024-05-27 18:15:59 -0600

    Juego cartas

---
+++ scripts.js
@@ -0,0 +1,70 @@
+const gameBoard = document.getElementById('game-board');
+const cardValues = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function createBoard() {
+    shuffle(cardValues);
+    cardValues.forEach(value => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <div class="front"></div>
+            <div class="back">${value}</div>
+        `;
+        card.addEventListener('click', flipCard);
+        gameBoard.appendChild(card);
+    });
+}
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.innerHTML === secondCard.innerHTML;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 400);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+function shuffle(array) {
+    array.sort(() => 0.5 - Math.random());
+}
+
+createBoard();
